a965200f89ddd6cc291f7bf17e1ca30b57057c1b
Sebastian Fält Barbalace <sfa0110@hotmail.com>
2024-04-15 11:56:40 +0200

    hej

--- Data/Context/ApplicationDbContext.cs
+++ Data/Context/ApplicationDbContext.cs
@@ -1,22 +1,21 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Echo_HemAPI.Data.Models;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace Echo_HemAPI.Data.Context
 {
     public class ApplicationDbContext : IdentityDbContext<Realtor>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-    {
+        {
+
+        }
+    
         public DbSet<County> Counties { get; set; }
         public DbSet<Estate> Estates { get; set; }
         public DbSet<RealtorFirm> RealtorFirms { get; set; }
-
-        }
         public DbSet<Realtor> Realtors { get; set; }
 
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        
     }
 }
