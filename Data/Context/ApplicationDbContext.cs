c6826ae17da277d20788021818dd34c65b7c8110
Sebastian Fält Barbalace <sfa0110@hotmail.com>
2024-04-15 15:41:40 +0200

    small stuff and start of county seed method

--- Data/Context/ApplicationDbContext.cs
+++ Data/Context/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Echo_HemAPI.Data.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Echo_HemAPI.Helper;
 
 namespace Echo_HemAPI.Data.Context
 {
@@ -11,12 +12,17 @@ namespace Echo_HemAPI.Data.Context
         {
 
         }
-    
+
         public DbSet<County> Counties { get; set; }
         public DbSet<Estate> Estates { get; set; }
         public DbSet<RealtorFirm> RealtorFirms { get; set; }
         public DbSet<Realtor> Realtors { get; set; }
 
-        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var counties = SeedCounties.GetCounties();
+            modelBuilder.Entity<County>().HasData(counties);
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
--- Data/Models/County.cs
+++ Data/Models/County.cs
@@ -1,6 +1,15 @@
-﻿namespace Echo_HemAPI.Data.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Echo_HemAPI.Data.Models
 {
     public class County
     {
+        [Required]
+        public int CountyId { get; set; }
+        [Required]
+        [DisplayName("County")]
+        public string CountyName { get; set; } = string.Empty;
+        
     }
 }
