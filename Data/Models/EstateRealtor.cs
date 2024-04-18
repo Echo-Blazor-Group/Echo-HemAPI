f22992b81037c797bcebdfb10ecf2ccb16d1542c
gustaf frejd <gustaf.frejd@hotmail.com>
2024-04-18 13:19:58 +0200

    added two helper classes for the tables might delete them later

---
+++ Data/Models/EstateRealtor.cs
@@ -0,0 +1,13 @@
+﻿namespace Echo_HemAPI.Data.Models
+{
+    public class EstateRealtor
+    {
+        public int EstateId { get; set; }
+        public int RealtorId { get; set; }
+
+        public Estate Estate { get; set; }
+        public Realtor Realtor { get; set; }
+
+
+    }
+}
