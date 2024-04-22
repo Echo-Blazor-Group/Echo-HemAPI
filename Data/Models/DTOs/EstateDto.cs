fbaa40b47070024e8160fc4df8c19767a16354b4
gustaf frejd <gustaf.frejd@hotmail.com>
2024-04-22 10:51:20 +0200

    added DTO object

---
+++ Data/Models/DTOs/EstateDto.cs
@@ -0,0 +1,29 @@
+﻿namespace Echo_HemAPI.Data.Models.DTOs
+{
+    public class EstateDto
+    {
+        public int Id { get; set; }
+        
+        public string Address { get; set; } = string.Empty;
+       
+        public int StartingPrice { get; set; }
+        
+        public string LivingAreaKvm { get; set; } = string.Empty;
+       
+        public string NumberOfRooms { get; set; } = string.Empty;
+       
+        public string BiAreaKvm { get; set; } = string.Empty;
+       
+        public string EstateAreaKvm { get; set; } = string.Empty;
+       
+        public string MonthlyFee { get; set; } = string.Empty;
+        
+        public string RunningCosts { get; set; } = string.Empty;
+        
+        public string ConstructionDate { get; set; } = string.Empty;
+        
+        public string EstateDescription { get; set; } = string.Empty;
+       
+        public DateOnly? PublishDate { get; set; } = new DateOnly();
+    }
+}
