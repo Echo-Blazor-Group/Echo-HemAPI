9d4dae423bbab7db311e593339145537e5bf7e63
Sebastian Fält Barbalace <sfa0110@hotmail.com>
2024-04-22 08:49:46 +0200

    Added CRUD functionality to API-realtorcontroller.

--- Controllers/RealtorController.cs
+++ Controllers/RealtorController.cs
@@ -1,7 +1,12 @@
 ﻿using Echo_HemAPI.Data.Context;
 using Echo_HemAPI.Data.Models;
+using Echo_HemAPI.Data.Repositories.Interfaces;
+using Echo_HemAPI.Data.Repositories.Repos;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Echo_HemAPI.Controllers
 {
@@ -11,42 +16,159 @@ namespace Echo_HemAPI.Controllers
     {
         private readonly UserManager<Realtor> _userManager;
         private readonly ApplicationDbContext _context;
+        private readonly IEstateRepository _estateRepo;
 
-        public RealtorController(UserManager<Realtor> userManager, ApplicationDbContext context)
+        public RealtorController(UserManager<Realtor> userManager, ApplicationDbContext context, IEstateRepository estateRepo)
         {
             _userManager = userManager;
             _context = context;
+            _estateRepo = estateRepo;
         }
         // GET: api/<RealtorController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<IActionResult> GetAllAsync()
         {
-            return new string[] { "value1", "value2" };
+            var users = await _userManager.Users.ToListAsync();
+            if (users is not null && users.Count > 0)
+            {
+                return Ok(users);
+            }
+            else
+            {
+                return NotFound("There are no registered users yet.");
+            }
         }
 
         // GET api/<RealtorController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<IActionResult> GetByIdAsync(string id)
         {
-            return "value";
+            if (id.IsNullOrEmpty())
+            {
+                return NotFound("Invalid id.");
+            }
+
+
+            var user = await _userManager.Users.Where(u => u.Id == id).FirstOrDefaultAsync();
+            if (user is not null)
+            {
+                return Ok(user);
+            }
+            else
+            {
+                return NotFound("Invalid id.");
+            }
         }
 
         // POST api/<RealtorController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> AddAsync([FromBody] Realtor user)
         {
+            try
+            {
+                if (ModelState.IsValid && user.PasswordHash is not null)
+                {
+                    var realtor = new Realtor
+                    {
+                        UserName = user.UserName,
+                        Email = user.Email
+
+                    };
+                    var createdUser = await _userManager.CreateAsync(realtor, user.PasswordHash);
+                    if (createdUser.Succeeded)
+                    {
+                        return Ok(createdUser);
+                    }
+                    else
+                    {
+                        return StatusCode(500, createdUser.Errors);
+                    }
+                }
+                else
+                {
+                    return BadRequest(ModelState);
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex);
+            }
         }
 
         // PUT api/<RealtorController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult> EditAsync(string id, [FromBody] Realtor user)
         {
+            if (id != user.Id)
+            {
+                return NotFound("Inconsistent id's.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                var existingUser = await _userManager.Users.Where(i => i.Id == id).FirstOrDefaultAsync();
+                if (existingUser is not null && user.PasswordHash is not null)
+                {
+                    existingUser.Email = user.Email;
+                    existingUser.UserName = user.UserName;
+
+                    var resetToken = await _userManager.GeneratePasswordResetTokenAsync(existingUser);
+                    var resultPasswordChange = await _userManager.ResetPasswordAsync(existingUser, resetToken, user.PasswordHash);
+
+                    var result = await _userManager.UpdateAsync(existingUser);
+                    return Ok(existingUser);
+                }
+                else
+                {
+                    return NotFound("Existing user doesn't exist and/or new user.PasswordHash is null.");
+                }
+
+
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+
         }
 
         // DELETE api/<RealtorController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<IActionResult> RemoveAsync(string id)
         {
+            if (id.IsNullOrEmpty())
+            {
+                return NotFound("Invalid id.");
+            }
+
+
+            var user = await _userManager.Users.Where(u => u.Id == id).FirstOrDefaultAsync();
+            if (user is not null)
+            {
+                var estatesMatchingRealtorId = await _context.Estates.Where(e => e.Realtor.Id == user.Id).ToListAsync();
+                Picture? profilePictureAttachedToUser = await _context.Pictures.Where(p => p.RealtorId == user.Id).FirstOrDefaultAsync();
+                //await _estateRepo.FindAsync(e => e.Realtor.Id == user.Id);API error saying cant set up service estaterepo
+                if (estatesMatchingRealtorId.Count > 0)
+                {
+                    foreach (var estate in estatesMatchingRealtorId)
+                    {
+                        estate.Realtor = null;
+                    }
+                }
+                
+                user.RealtorFirm = null;
+                user.ProfilePicture = null;
+                profilePictureAttachedToUser = null;
+
+                await _context.SaveChangesAsync();
+                await _userManager.DeleteAsync(user);
+                return Ok(user);
+
+            }
+            else
+            {
+                return NotFound("Delete failed, user does not exist.");
+            }
         }
     }
 }
