d5620b468ab12cc37a0410993446a9037c6246f7
gustaf frejd <gustaf.frejd@hotmail.com>
2024-04-30 10:46:22 +0200

    added county and category controllers

---
+++ Data/Repositories/Interfaces/ICountyRepository.cs
@@ -0,0 +1,16 @@
+﻿using Echo_HemAPI.Data.Models;
+using System.Linq.Expressions;
+
+namespace Echo_HemAPI.Data.Repositories.Interfaces
+{
+    public interface ICountyRepository
+    {
+        Task<County> AddAsync(County entity);
+        Task<County> UpdateAsync(County entity);
+        Task<County?> GetByIdAsync(int id);
+        Task<County> RemoveAsync(County entity);
+        Task<IEnumerable<County>> GetAllAsync();
+        Task<IQueryable<County>> FindAsync(Expression<Func<County, bool>> predicate);
+        Task SaveChangesAsync();
+    }
+}
