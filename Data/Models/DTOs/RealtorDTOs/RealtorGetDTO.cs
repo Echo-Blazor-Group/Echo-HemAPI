6d348e8564b33a5a98d7e8cc4af7e090efdea20d
Sebastian Fält Barbalace <sfa0110@hotmail.com>
2024-05-08 22:07:19 +0200

    changed GetDTO slightly

--- Data/Models/DTOs/RealtorDTOs/RealtorGetDTO.cs
+++ Data/Models/DTOs/RealtorDTOs/RealtorGetDTO.cs
@@ -10,6 +10,7 @@ namespace Echo_HemAPI.Data.Models.DTOs.RealtorDTOs
         public string RealtorFirmName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string PhoneNumber { get; set; } = string.Empty;
+        public string ProfilePicture { get; set; } = "https://placehold.co/600x400/png";
         public bool IsActive { get; set; }
     }
 }
