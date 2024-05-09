c873dc78b8de05fded60e0b7c7ea7e8951f6c901
Sebastian Fält Barbalace <sfa0110@hotmail.com>
2024-05-09 21:01:56 +0200

    swapped out default profile picture and added nullable to one thing in realtorcontroller...

--- Data/Models/DTOs/RealtorDTOs/RealtorGetDTO.cs
+++ Data/Models/DTOs/RealtorDTOs/RealtorGetDTO.cs
@@ -10,7 +10,7 @@ namespace Echo_HemAPI.Data.Models.DTOs.RealtorDTOs
         public string RealtorFirmName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string PhoneNumber { get; set; } = string.Empty;
-        public string ProfilePicture { get; set; } = "https://placehold.co/600x400/png";
+        public string ProfilePicture { get; set; } = "https://shorturl.at/CJOR3";
         public bool IsActive { get; set; }
     }
 }
