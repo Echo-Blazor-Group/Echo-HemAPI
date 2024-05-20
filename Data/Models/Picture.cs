811b362c03e512019d5f9f2e019cd3aac6c2462e
gustaf frejd <gustaf.frejd@hotmail.com>
2024-05-20 19:23:20 +0200

    f

--- Data/Models/Picture.cs
+++ Data/Models/Picture.cs
@@ -7,7 +7,7 @@ namespace Echo_HemAPI.Data.Models
     {
         //Author Gustaf
         public int Id { get; set; }
-        public string PictureUrl { get; set; } = string.Empty;
+        public string PictureUrl { get; set; } = "https://static-00.iconduck.com/assets.00/no-image-icon-512x512-lfoanl0w.png";
         //EstateId is nullable because otherwise the DbSeeder will not work. But in the end a picture will always be created 
         //directly to houses unless created in the database itself.
         public int? EstateId { get; set; }
